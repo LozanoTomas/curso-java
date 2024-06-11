40486505412c2945353acb2d68e4d5d0ef412ab6
Tomas Lozano <lozanotdlc@gmail.com>
2024-06-10 22:34:55 -0300

    clase 1

---
+++ variables.js
@@ -0,0 +1,35 @@
+// variables y tipos de datos primitivos
+
+let nombre = "pepe" // string
+let apellido = "perez"
+
+// alert(nombre)
+
+console.log(nombre)
+// console.log(apellido)
+
+const documento = 123123132123 // number
+
+nombre = "yoyito"
+
+console.log(nombre)
+
+let EdadDeLaPersona = 31
+console.log (EdadDeLaPersona)
+
+//let numero = prompt("porfavor ingresa un numero")
+
+//console.log (numero)
+
+//let NombreDeUsuario = prompt("porfavor inicia sesion con tu nombre")
+
+//alert("hola " + NombreDeUsuario + " como estas?")
+
+let n1 = Number(prompt("ingresa el primer numero"))
+let n2 = Number(prompt("ingresa el segundo numero"))
+let total = n1 + n2
+console.log(total)
+
+// boolean 0 / 1 booleanos true o false
+
+console.log(n1 > n2)
